9f32e9ff85ae7acd397dbaecc77edae5553246b7
wanderbot11 <71700915+wanderbot11@users.noreply.github.com>
2021-11-20 14:21:48 -0500

    various changes

--- Autonomous/Auto_Base.java
+++ Autonomous/Auto_Base.java
@@ -7,11 +7,15 @@ public abstract class Auto_Base extends LinearOpMode {
     protected org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus auto_motors = new org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus();
     protected org.firstinspires.ftc.teamcode.Carousel_Call auto_carousel = new org.firstinspires.ftc.teamcode.Carousel_Call();
     protected org.firstinspires.ftc.teamcode.Distance_Sensor distance_sensor = new org.firstinspires.ftc.teamcode.Distance_Sensor();
+    protected org.firstinspires.ftc.teamcode.Arm_15455 arm = new org.firstinspires.ftc.teamcode.Arm_15455();
+    protected org.firstinspires.ftc.teamcode.Intake_15455 intake = new org.firstinspires.ftc.teamcode.Intake_15455();
 
     protected int inchToTicks = 91;
 
     public void init_classes() {
         auto_motors.init_auto_drive_motors(hardwareMap, telemetry);
         auto_carousel.init_carousel(hardwareMap, "carousel", false);
+        arm.init_arm(hardwareMap, "arm");
+        intake.init_intake(hardwareMap, "intake");
     }
 }
--- Autonomous/Auto_Blue_Left_15.java
+++ Autonomous/Auto_Blue_Left_15.java
@@ -2,12 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareDevice;
-
-import org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus;
 
 @Autonomous
 
@@ -18,19 +12,26 @@ public class Auto_Blue_Left_15 extends org.firstinspires.ftc.teamcode.Auto_Base 
     public void runOpMode() throws InterruptedException {
         this.init_classes();
 
+
         waitForStart();
 
-        auto_motors.goToSpot(inchToTicks*39,1);
+//        auto_motors.goToSpot(inchToTicks*39,1);
+        arm.autoArmUp(2);
+        intake.autoEject();
+        arm.autoArmDown();
+
+
 
         //raise arm
             //level 1: no strafe
             //level 2: strafe 2" right
             //level 3: strafe 5" right
+
         //output freight
 
-        auto_motors.goToSpot(inchToTicks*-18,1);
-        auto_motors.turn90left(1);
-        auto_motors.goToSpot(inchToTicks*36,1);
+//        auto_motors.goToSpot(inchToTicks*-18,1);
+//        auto_motors.turn90left(1);
+//        auto_motors.goToSpot(inchToTicks*36,1);
 
     }
 }
