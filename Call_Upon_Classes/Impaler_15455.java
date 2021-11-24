412d1cf9ac9c4e316e4004a52c481094e697cdd3
Laptop1 <ftcroborioles2021@gmail.com>
2021-11-23 17:58:44 -0800

    implemented the IMPALER >:D

---
+++ Call_Upon_Classes/Impaler_15455.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Impaler_15455 {
+    private Servo impaler = null;
+    private Telemetry telemetry = null;
+
+    public void init_impaler (HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        this.impaler = hardwareMap.get(Servo.class, "impaler");
+    }
+
+    public void control_impaler (Gamepad gamepad) {
+        boolean up = gamepad.y;
+        boolean down = gamepad.a;
+
+        if(up) {
+            impaler.setPosition(90); //?????
+        } else if (down) {
+            impaler.setPosition(0);
+        }
+    }
+
+    public void getTelemetry() {
+
+    }
+}
