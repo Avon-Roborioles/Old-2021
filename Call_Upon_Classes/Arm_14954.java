d595a2342ace91985b37628318e50a3ba95f5c87
Stephen-Onochie <87861480+Stephen-Onochie@users.noreply.github.com>
2021-12-01 23:02:22 -0500

    Finished Test Code for Passive Position Holder
    This new method sets both armlift motors to run to a target position and hold their place unless a new target is made.
    - Will need to test before competition (Ideally Thursday 12/2/21)

--- Call_Upon_Classes/Arm_14954.java
+++ Call_Upon_Classes/Arm_14954.java
@@ -14,22 +14,45 @@ public class Arm_14954 {
     private DcMotor armlift1 = null;
     private DcMotor armlift2 = null;
     private double speed;
+    
 
     public void init_armlift (HardwareMap map, String name1, String name2) {
         armlift1  = map.get(DcMotor.class, name1);
         armlift2  = map.get(DcMotor.class, name2);
     }
     
+    
+    
+    
+    
+    
       //Test method for normal armlift operation + Passive motor position holder  
       public void init_armliftv2 (HardwareMap map, String name1, String name2) {
+        //names motors for configurations
         armlift1  = map.get(DcMotor.class, name1);
         armlift2  = map.get(DcMotor.class, name2);
-          
+        
+        //sets current motor encoder values to zero (resets)
         armlift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armlift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        //tells motors to go to target position and hold it (key to position holder)
+        armlift1.setTargetPosition(0);
+        armlift2.setTargetPosition(0);
+          
+        armlift1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armlift2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
        
+        //maximum power robot can use to move and adjust armlift
+        armlift1.setPower(1);
+        armlift2.setPower(1);
     }
 
+    
+    
+    
+    
+    
     public void run_arm(Gamepad gp, Telemetry telemetry) {
 
         double ltrigger = gp.left_trigger;
@@ -49,26 +72,38 @@ public class Arm_14954 {
         get_telemetry(telemetry);
     }
     
+    
+    
+    
+    
+    
     //Test method for normal armlift operation + Passive motor position holder  
     public void run_armv2 (Gamepad gp, Telemetry telemetry) {
         
         double ltrigger = gp.left_trigger;
         double rtrigger = gp.right_trigger;
         
+        //moves armlift up
         if (ltrigger > 0) {
-            speed = 1;
+            
+            armlift1.setTargetPosition( getCurrentPosition() + 1 ); //change number if needed
+            armlift2.setTargetPosition( getCurrentPosition() + 1 ); //change number if needed
+         
+        //moves armlift down
         } else if (rtrigger > 0 ) {
-            speed = -1;
-        } else {
-            speed = 0;
+            
+            armlift1.setTargetPosition( getCurrentPosition() - 1 ); //change number if needed
+            armlift2.setTargetPosition( getCurrentPosition() - 1 ); //change number if needed
+            
         }
 
-        armlift1.setPower(speed);
-        armlift2.setPower(speed);
-
+        
         get_telemetry(telemetry);
     }   
 
+    
+    
+    
     
     
     
