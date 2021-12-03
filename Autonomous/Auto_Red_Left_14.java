89cadeeda0cd22ae64cf78bbd881cf7f69d7de62
Roborioles <30003292@students.avon-schools.org>
2021-12-02 19:59:06 -0500

    Stephen's Changes 12/2/21

--- Autonomous/Auto_Red_Left_14.java
+++ Autonomous/Auto_Red_Left_14.java
@@ -17,16 +17,13 @@ public class Auto_Red_Left_14 extends org.firstinspires.ftc.teamcode.Auto_Base_1
 
         // this.sense_barcode();
 
-        auto_motors.strafeLeft(0.8,8);
+        auto_motors.strafeLeft(0.8,6);
         auto_motors.goToSpot((int) (-inchToTicks*25), .75);
-        //auto_motors.turn90left(1);
 
         auto_carousel.run_carousel_auto(telemetry,true);
 
         auto_motors.strafeLeft(.75, 20);
-        auto_motors.goToSpot((int) (-inchToTicks*3), .75);
-        // auto_motors.goToSpot(-inchToTicks*9,.75);
-        // auto_motors.strafeLeft(.75, 15);
+        auto_motors.goToSpot((int) (-inchToTicks*6), .75);
 
 
 
